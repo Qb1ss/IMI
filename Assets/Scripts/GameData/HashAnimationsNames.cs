ee0c88f3955a82756f7f277b1e24c3be717d5288
Qb1ss <gleb.bortsov79@gmail.com>
2021-12-26 19:12:03 +1000

    Передвижение и анимация персонажа

---
+++ Assets/Scripts/GameData/HashAnimationsNames.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class HashAnimationsNames : MonoBehaviour
+{
+    public int RunBoolHash = Animator.StringToHash("IsRun");
+    public int WalkBoolHash = Animator.StringToHash("IsWalk");
+}
